85eced279b36732c348c4f010eea86c25f1ad4ab
SuperBeast007 <henrupaxton11@gmail.com>
2023-09-05 12:38:12 +0200

    Database connection commecnted out (not working)

--- Views/AddRoom.xaml.cs
+++ Views/AddRoom.xaml.cs
@@ -1,3 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ReloCheck.Resources.ViewModels;
+using ReloCheck.Views;
+using Microsoft.Maui.Controls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
 namespace ReloCheck.Views;
 
 public partial class AddRoom : ContentPage
@@ -7,8 +20,33 @@ public partial class AddRoom : ContentPage
 		InitializeComponent();
 	}
 
-	private async void OnSaveButton(object sender, EventArgs e)
+    string MyConnectionString = @"server=LAPTOP-UPC09RTU;userid=default;password=default;database=MoverSynq ";
+
+
+    private async void OnSaveButton(object sender, EventArgs e)
 	{
-        await Navigation.PushAsync(new RoomDetailss());
+
+		//string RoomName = RoomEntry.Text;
+		
+		//MySqlConnection connection = new MySqlConnection(MyConnectionString);
+		//MySqlCommand cmd;
+		//connection.Open();
+
+		//try
+		//{
+		//	cmd = connection.CreateCommand();
+		//	cmd.CommandText = "INSERT INTO ROOMS(Name)VALUES(@Name)";
+		//	cmd.Parameters.AddWithValue("@Name", RoomEntry.Text);
+		//	cmd.ExecuteNonQuery();
+		//} catch (Exception) 
+		//{
+		//	throw;
+		//} finally
+		//{
+		//	if (connection.State == ConnectionState.Open)
+		//	{
+		//		connection.Close();
+		//	}
+		//}
     }
 }
--- Views/RoomDetails.xaml.cs
+++ Views/RoomDetails.xaml.cs
@@ -1,9 +1,16 @@
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
 using ReloCheck.Resources.ViewModels;
 using ReloCheck.Views;
 using Microsoft.Maui.Controls;
 using MySql.Data.MySqlClient;
 using System.Data;
+using DevExpress.XtraReports.Design;
 
 namespace ReloCheck.Views;
 
@@ -21,45 +28,62 @@ public partial class RoomDetailss : ContentPage
         await Navigation.PushAsync(new AddRoom());
 
     }
+    string MyConnectionString = @"server=LAPTOP-UPC09RTU;userid=default;password=default;database=MoverSynq ";
 
     private void OnReloadClick(object sender, EventArgs ex)
     {
-        string connstring = @"server=LAPTOP-UPC09RTU;userid=default;password=default;database=MoverSynq ";
+        //try
+        //{
+        //    string Query = "SELECT * FROM ROOM.MoverSynq;";
+        //    MySqlConnection MyConn = new MySqlConnection(MyConnectionString);
+        //    MySqlCommand MyCommand = new MySqlCommand(Query, MyConn);
 
-        MySqlConnection conn = null;
+        //    MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+        //    MyAdapter.SelectCommand = MyCommand;
+        //    DataTable dTable = new DataTable();
+        //    MyAdapter.Fill(dTable);
+        //} catch (Exception ez)
+        //{
+        //    throw;
+        //}
 
-        try
-        {
-            conn = new MySqlConnection(connstring);
-            conn.Open();
-
-            string list = "SELECT * FROM Room";
-            MySqlDataAdapter da = new MySqlDataAdapter(list, conn);
-            DataSet ds = new DataSet();
-            da.Fill(ds, "Room");
-            DataTable dt = ds.Tables["Room"];
-
-            foreach (DataRow row in dt.Rows)
-            {
-                foreach (DataColumn col in dt.Columns)
-                {
-                    Console.Write("\n");
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Error: {0}", e.ToString());
-        }
-        finally
-        {
-            if (conn != null)
-            {
-                conn.Close();
-            }
-        }
+        
     }
 
 }
 
 
+//string MyConnectionString = @"server=LAPTOP-UPC09RTU;userid=default;password=default;database=MoverSynq ";
+
+//MySqlConnection conn = null;
+
+//        try
+//        {
+//            conn = new MySqlConnection(MyConnectionString);
+//conn.Open();
+
+//            string list = "SELECT * FROM Room";
+//MySqlDataAdapter da = new MySqlDataAdapter(list, conn);
+//DataSet ds = new DataSet();
+//da.Fill(ds, "Room");
+//            DataTable dt = ds.Tables["Room"];
+
+//            foreach (DataRow row in dt.Rows)
+//            {
+//                foreach (DataColumn col in dt.Columns)
+//                {
+//                    Console.Write("\n");
+//                }
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//    Console.WriteLine("Error: {0}", e.ToString());
+//}
+//        finally
+//        {
+//    if (conn != null)
+//    {
+//        conn.Close();
+//    }
+//}
